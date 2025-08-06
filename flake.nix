c10164f72b490913cac271e09e4c13a0957911b4
Luke <luke@maraud.earth>
2025-08-06 15:03:23 -0400

    created basic flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "biter development environment";
+
+  # I have chosen not to manage Rust toolchain/dependencies via Nix, because I am quite
+  # fond of rustup, and the entire Rust toolchain. This flake will be for external deps.
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    macpkgs = nixpkgs.legacyPackages.aarch64-darwin;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [ clang ];
+    };
+  };
+}
